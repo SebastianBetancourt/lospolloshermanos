a2fbd857425641c7ff27685874acc5d66ff58e13
MurphLaws <lasso.nicolas@correounivalle.edu.co>
2021-04-11 22:43:00 -0500

    Clases modificadas

--- orm-test.js
+++ orm-test.js
@@ -9,54 +9,284 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
+exports.Venta = exports.Cliente = exports.Producto_venta = exports.Producto = void 0;
 require("reflect-metadata");
 // Entidades
 const typeorm_1 = require("typeorm");
-let User = class User {
+/*@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+    
+    @Column()
+    isActive: boolean;
+}*/
+//import {Producto} from "./Producto";
+//import {Venta} from "./Venta";
+let Producto = class Producto {
 };
 __decorate([
     typeorm_1.PrimaryGeneratedColumn(),
     __metadata("design:type", Number)
-], User.prototype, "id", void 0);
+], Producto.prototype, "id", void 0);
 __decorate([
     typeorm_1.Column(),
     __metadata("design:type", String)
-], User.prototype, "firstName", void 0);
+], Producto.prototype, "nombre", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Producto.prototype, "descripcion", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", Number)
+], Producto.prototype, "precio_unitario", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Producto.prototype, "categoria", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", Number)
+], Producto.prototype, "porcentaje_iva", void 0);
 __decorate([
     typeorm_1.Column(),
     __metadata("design:type", Boolean)
-], User.prototype, "isActive", void 0);
-User = __decorate([
+], Producto.prototype, "disponible", void 0);
+__decorate([
+    typeorm_1.OneToMany(() => Producto_venta, Producto_venta => Producto_venta.producto),
+    __metadata("design:type", Array)
+], Producto.prototype, "producto_venta", void 0);
+Producto = __decorate([
     typeorm_1.Entity()
-], User);
-exports.User = User;
+], Producto);
+exports.Producto = Producto;
+let Producto_venta = class Producto_venta {
+};
+__decorate([
+    typeorm_1.PrimaryGeneratedColumn(),
+    __metadata("design:type", Number)
+], Producto_venta.prototype, "id", void 0);
+__decorate([
+    typeorm_1.ManyToOne(() => Producto, producto => producto.id),
+    __metadata("design:type", Producto)
+], Producto_venta.prototype, "producto", void 0);
+__decorate([
+    typeorm_1.ManyToOne(() => Venta, venta => venta.id),
+    __metadata("design:type", Array)
+], Producto_venta.prototype, "venta", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", Number)
+], Producto_venta.prototype, "cantidad", void 0);
+Producto_venta = __decorate([
+    typeorm_1.Entity()
+], Producto_venta);
+exports.Producto_venta = Producto_venta;
+let Cliente = class Cliente {
+};
+__decorate([
+    typeorm_1.PrimaryGeneratedColumn(),
+    __metadata("design:type", Number)
+], Cliente.prototype, "id", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Cliente.prototype, "identificacion", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Cliente.prototype, "nombre", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Cliente.prototype, "telefono", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Cliente.prototype, "fecha_de_nacimiento", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Cliente.prototype, "direccion", void 0);
+__decorate([
+    typeorm_1.OneToMany(() => Venta, venta => venta.cliente),
+    __metadata("design:type", Array)
+], Cliente.prototype, "venta", void 0);
+Cliente = __decorate([
+    typeorm_1.Entity()
+], Cliente);
+exports.Cliente = Cliente;
+let Venta = class Venta {
+};
+__decorate([
+    typeorm_1.PrimaryGeneratedColumn(),
+    __metadata("design:type", Number)
+], Venta.prototype, "id", void 0);
+__decorate([
+    typeorm_1.ManyToOne(() => Cliente, cliente => cliente.id),
+    __metadata("design:type", Cliente)
+], Venta.prototype, "cliente", void 0);
+__decorate([
+    typeorm_1.Column(),
+    __metadata("design:type", String)
+], Venta.prototype, "fecha_de_venta", void 0);
+__decorate([
+    typeorm_1.ManyToMany(type => Producto),
+    typeorm_1.JoinTable(),
+    __metadata("design:type", Array)
+], Venta.prototype, "productos", void 0);
+Venta = __decorate([
+    typeorm_1.Entity()
+], Venta);
+exports.Venta = Venta;
 // Conexión
 const typeorm_2 = require("typeorm");
 typeorm_2.createConnection({
     type: 'postgres',
     url: 'postgres://fnwprpro:SxXTT1W46Ol7iJddsfzpdTrCeI7043Eu@tuffi.db.elephantsql.com:5432/fnwprpro',
-    entities: [User],
+    entities: [Producto, Venta, Cliente, Producto_venta],
     synchronize: true
 }).then(async (conexion) => {
     // Queries
     // Insert
     await conexion
-        .createQueryBuilder()
-        .insert()
-        .into(User)
-        .values([
-        { firstName: "Timber", isActive: true },
-        { firstName: "Phantom", isActive: false }
-    ])
-        .execute();
+        .createQueryBuilder();
+    /*       .insert()
+           .into(Cliente)
+           .values([
+               {identificacion: "1106011547",
+                nombre: "Daniel Ramirez",
+                telefono: "3158477452",
+                fecha_de_nacimiento: "22-01-2001",
+                direccion: "Cra 2, #43-67"},
+   
+                {identificacion: "115489878",
+                nombre: "Carlos Mol",
+                telefono: "3021547895",
+                fecha_de_nacimiento: "07-01-2002",
+                direccion: "Cra 10, #15-89"},
+   
+   
+                {identificacion: "1154878988",
+                nombre: "Joab Arias",
+                telefono: "3062544125",
+                fecha_de_nacimiento: "15-04-2000",
+                direccion: "Cra 7, #68-77"},
+   
+   
+                {identificacion: "1123265455",
+                nombre: "Eduardo Fernandez",
+                telefono: "3652541848",
+                fecha_de_nacimiento: "14-08-1956",
+                direccion: "Cra 9, #23-21"},
+           ])
+           .execute();
+   
+   
+           await conexion
+           .createQueryBuilder()
+           .insert()
+           .into(Producto)
+           .values([
+               {nombre: "Pollo Apanado",
+                descripcion: "Pollo con una deliciosa y crujiente cobertura espacial, servido con miel",
+                precio_unitario: 5000,
+                categoria: "pollo",
+                porcentaje_iva: 19,
+                disponible: true},
+   
+                {nombre: "CocaCola",
+                descripcion: "Bedida artificial con demasiada azucar de Cola  muy refrescante",
+                precio_unitario: 20000,
+                categoria: "bebidas",
+                porcentaje_iva: 19,
+                disponible: true},
+   
+                {nombre: "Pollo Asado",
+                descripcion: "Pollo completo asado al carbon con especias especiales",
+                precio_unitario: 15000,
+                categoria: "pollo",
+                porcentaje_iva: 15,
+                disponible: true},
+   
+                {nombre: "Papas a la Francesa",
+                descripcion: "Papas fritas con la receta de la casa, crujientes por fuera, suaves por dentro",
+                precio_unitario: 6000,
+                categoria: "acompañamientos",
+                porcentaje_iva: 19,
+                disponible: true}
+           ])
+           .execute();
+   
+   
+           await conexion
+           .createQueryBuilder()
+           .insert()
+           .into(Venta)
+           .values([
+               {cliente: await conexion.createQueryBuilder().select("cliente")
+               .from(Cliente, "cliente").where("cliente.id = 1").getOne(),
+               fecha_de_venta: "21-05-2021",
+               productos:
+               await conexion.createQueryBuilder().select("producto")
+               .from(Producto, "producto").getMany()
+               },
+   
+               {cliente: await conexion.createQueryBuilder().select("cliente")
+               .from(Cliente, "cliente").where("cliente.id = 1").getOne(),
+               fecha_de_venta: "20-06-2021",
+               productos:
+               [await conexion.createQueryBuilder().select("producto")
+               .from(Producto, "producto").where("producto.id = 3").getOneOrFail()]
+               },
+   
+               {cliente: await conexion.createQueryBuilder().select("cliente")
+               .from(Cliente, "cliente").where("cliente.id = 2").getOne(),
+               fecha_de_venta: "15-01-2021",
+               productos:
+               [await conexion.createQueryBuilder().select("producto")
+               .from(Producto, "producto").where("producto.id = 2 or producto.id = 1").getOneOrFail()]
+               },
+   
+   
+               {cliente: await conexion.createQueryBuilder().select("cliente")
+               .from(Cliente, "cliente").where("cliente.id = 3").getOne(),
+               fecha_de_venta: "01-02-2021",
+               productos:
+               [await conexion.createQueryBuilder().select("producto")
+               .from(Producto, "producto").where("producto.id = 2 or producto.id = 3").getOneOrFail()]
+               },
+   
+               {cliente: await conexion.createQueryBuilder().select("cliente")
+               .from(Cliente, "cliente").where("cliente.id = 4").getOne(),
+               fecha_de_venta: "13-04-2021",
+               productos:
+               [await conexion.createQueryBuilder().select("producto")
+               .from(Producto, "producto").where("producto.id = 3").getOneOrFail()]
+               },
+               
+               
+           ])
+           .execute();
+   
+   
+    */
     // Select
-    // get all users (usando QueryBuilder)
-    const users = await conexion.createQueryBuilder().select("user").from(User, "user").getMany();
+    // get all clientes
+    //await conexion.createQueryBuilder().delete()
+    //.from(Producto, "producto").where("precio_unitario <= 6000").execute();
+    const producto = await conexion.createQueryBuilder().select("producto")
+        .from(Producto, "producto").where("id = 2").getMany();
+    console.log(producto);
     // users es un array de JSONs que representan Users
-    console.log(users);
     //Filter 
     // get users that are not active (usando managers)
-    const nonActiveUsers = await conexion.manager.find(User, { where: { isActive: false } });
-    console.log(nonActiveUsers[0].firstName);
+    //const cumplenEnEnero : Cliente[] = await conexion.manager
+    //.find(Cliente, { where: { fecha_de_nacimiento: Like("___01%") }});
+    //console.log(cumplenEnEnero);
 });
--- orm-test.ts
+++ orm-test.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
 // Entidades
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToOne, JoinColumn, OneToMany, LessThanOrEqual, JoinTable, ManyToMany, Like, SimpleConsoleLogger} from "typeorm";
 
-@Entity()
+/*@Entity()
 export class User {
 
     @PrimaryGeneratedColumn()
@@ -13,7 +13,92 @@ export class User {
 	
 	@Column()
     isActive: boolean;
+}*/
+
+
+//import {Producto} from "./Producto";
+//import {Venta} from "./Venta";
+
+
+
+
+@Entity()
+export class Producto{
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    nombre: string;
+    @Column()
+    descripcion: string;
+    @Column()
+    precio_unitario: number;
+    @Column()
+    categoria: string;
+    @Column()
+    porcentaje_iva: number;
+    @Column()
+    disponible: boolean;
+    @OneToMany(() => Producto_venta, Producto_venta => Producto_venta.producto)
+    producto_venta: Producto_venta[];
 }
+
+
+
+@Entity()
+export class Producto_venta{
+    @PrimaryGeneratedColumn()
+    id: number;
+    @ManyToOne(() => Producto, producto => producto.id)
+    producto: Producto;
+    @ManyToOne(() => Venta, venta => venta.id)
+    venta: Venta[];
+    @Column()
+    cantidad: number;
+}
+
+
+
+
+@Entity()
+export class Cliente{
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    identificacion: string;
+    @Column()
+    nombre: string;
+    @Column()
+    telefono: string;
+    @Column() 
+    fecha_de_nacimiento: string;
+    @Column()
+    direccion: string;
+    @OneToMany(() => Venta, venta => venta.cliente)
+    venta: Venta[];
+}
+
+
+
+@Entity()
+export class Venta{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => Cliente, cliente => cliente.id)
+    cliente: Cliente;
+
+    @Column() 
+    fecha_de_venta: string;
+
+    @ManyToMany(type => Producto)
+    @JoinTable()
+    productos: Producto[];
+}
+
+
+
+
 
 // Conexión
 import {createConnection, Connection} from "typeorm";
@@ -21,7 +106,7 @@ import {createConnection, Connection} from "typeorm";
 createConnection({
     type: 'postgres',
     url: 'postgres://fnwprpro:SxXTT1W46Ol7iJddsfzpdTrCeI7043Eu@tuffi.db.elephantsql.com:5432/fnwprpro',
-	entities: [User],
+	entities: [Producto, Venta, Cliente, Producto_venta],
     synchronize: true
 }).then(async (conexion) => {
 
@@ -29,23 +114,141 @@ createConnection({
     // Insert
     await conexion
         .createQueryBuilder()
-        .insert()
-        .into(User)
+ /*       .insert()
+        .into(Cliente)
         .values([
-            { firstName: "Timber", isActive : true }, 
-            { firstName: "Phantom",  isActive : false }
+            {identificacion: "1106011547",
+             nombre: "Daniel Ramirez", 
+             telefono: "3158477452", 
+             fecha_de_nacimiento: "22-01-2001",
+             direccion: "Cra 2, #43-67"},
+
+             {identificacion: "115489878",
+             nombre: "Carlos Mol", 
+             telefono: "3021547895", 
+             fecha_de_nacimiento: "07-01-2002",
+             direccion: "Cra 10, #15-89"},
+
+
+             {identificacion: "1154878988",
+             nombre: "Joab Arias", 
+             telefono: "3062544125", 
+             fecha_de_nacimiento: "15-04-2000",
+             direccion: "Cra 7, #68-77"},
+
+
+             {identificacion: "1123265455",
+             nombre: "Eduardo Fernandez", 
+             telefono: "3652541848", 
+             fecha_de_nacimiento: "14-08-1956",
+             direccion: "Cra 9, #23-21"},
         ])
         .execute();
 
+
+        await conexion
+        .createQueryBuilder()
+        .insert()
+        .into(Producto)
+        .values([
+            {nombre: "Pollo Apanado", 
+             descripcion: "Pollo con una deliciosa y crujiente cobertura espacial, servido con miel", 
+             precio_unitario: 5000,
+             categoria: "pollo",
+             porcentaje_iva: 19,
+             disponible: true},
+
+             {nombre: "CocaCola", 
+             descripcion: "Bedida artificial con demasiada azucar de Cola  muy refrescante", 
+             precio_unitario: 20000,
+             categoria: "bebidas",
+             porcentaje_iva: 19,
+             disponible: true},
+
+             {nombre: "Pollo Asado", 
+             descripcion: "Pollo completo asado al carbon con especias especiales", 
+             precio_unitario: 15000,
+             categoria: "pollo",
+             porcentaje_iva: 15,
+             disponible: true},
+
+             {nombre: "Papas a la Francesa", 
+             descripcion: "Papas fritas con la receta de la casa, crujientes por fuera, suaves por dentro", 
+             precio_unitario: 6000,
+             categoria: "acompañamientos",
+             porcentaje_iva: 19,
+             disponible: true}
+        ])
+        .execute();
+
+
+        await conexion
+        .createQueryBuilder()
+        .insert()
+        .into(Venta)
+        .values([
+            {cliente: await conexion.createQueryBuilder().select("cliente")
+            .from(Cliente, "cliente").where("cliente.id = 1").getOne(),
+            fecha_de_venta: "21-05-2021",
+            productos: 
+            await conexion.createQueryBuilder().select("producto")
+            .from(Producto, "producto").getMany()
+            },
+
+            {cliente: await conexion.createQueryBuilder().select("cliente")
+            .from(Cliente, "cliente").where("cliente.id = 1").getOne(),
+            fecha_de_venta: "20-06-2021",
+            productos: 
+            [await conexion.createQueryBuilder().select("producto")
+            .from(Producto, "producto").where("producto.id = 3").getOneOrFail()]
+            },
+
+            {cliente: await conexion.createQueryBuilder().select("cliente")
+            .from(Cliente, "cliente").where("cliente.id = 2").getOne(),
+            fecha_de_venta: "15-01-2021",
+            productos: 
+            [await conexion.createQueryBuilder().select("producto")
+            .from(Producto, "producto").where("producto.id = 2 or producto.id = 1").getOneOrFail()]
+            },
+
+
+            {cliente: await conexion.createQueryBuilder().select("cliente")
+            .from(Cliente, "cliente").where("cliente.id = 3").getOne(),
+            fecha_de_venta: "01-02-2021",
+            productos: 
+            [await conexion.createQueryBuilder().select("producto")
+            .from(Producto, "producto").where("producto.id = 2 or producto.id = 3").getOneOrFail()]
+            },
+
+            {cliente: await conexion.createQueryBuilder().select("cliente")
+            .from(Cliente, "cliente").where("cliente.id = 4").getOne(),
+            fecha_de_venta: "13-04-2021",
+            productos: 
+            [await conexion.createQueryBuilder().select("producto")
+            .from(Producto, "producto").where("producto.id = 3").getOneOrFail()]
+            },
+            
+            
+        ])
+        .execute();
+
+
+ */
     // Select
-    // get all users (usando QueryBuilder)
-    const users : User[] = await conexion.createQueryBuilder().select("user").from(User, "user").getMany();
+    // get all clientes
+    //await conexion.createQueryBuilder().delete()
+    //.from(Producto, "producto").where("precio_unitario <= 6000").execute();
 
+    const producto : Producto[] = await conexion.createQueryBuilder().select("producto")
+    .from(Producto, "producto").where("id = 2").getMany();
+
+    console.log(producto)
     // users es un array de JSONs que representan Users
-    console.log(users);
-
+   
+   
     //Filter 
     // get users that are not active (usando managers)
-    const nonActiveUsers : User[] = await conexion.manager.find(User, { where: { isActive : false }});
-    console.log(nonActiveUsers[0].firstName);
+    //const cumplenEnEnero : Cliente[] = await conexion.manager
+    //.find(Cliente, { where: { fecha_de_nacimiento: Like("___01%") }});
+    //console.log(cumplenEnEnero);
 });
