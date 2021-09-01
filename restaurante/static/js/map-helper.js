c5336512c8dc54ad14e3fbc41f85c4b2a68e42b5
MurphLaws <lasso.nicolas@correounivalle.edu.co>
2021-09-01 18:24:45 -0500

    Added static scripts

--- restaurante/static/js/map-helper.js
+++ restaurante/static/js/map-helper.js
@@ -1,7 +1,7 @@
 		function initialize() {
 		  var map = new google.maps.Map(
 			document.getElementById('map-canvas'), {
-			  center: new google.maps.LatLng(37.4419, -122.1419),
+			  center: new google.maps.LatLng(35.08449, -106.65114),
 			  scrollwheel: false,
 			  zoom: 13,
 			  mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -9,7 +9,7 @@
 		  });
 
 		  var marker = new google.maps.Marker({
-				position: new google.maps.LatLng(37.4419, -122.1419),
+				position: new google.maps.LatLng(35.08449, -106.65114),
 				map: map
 		  });
 
