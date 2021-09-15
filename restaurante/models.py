b7038ff14ea2fb6ad3f754268d1f4be8e3c7d96c
Sebastian Betancourt <jsb998@hotmail.com>
2021-09-14 21:34:37 -0500

    mas tablas

--- restaurante/models.py
+++ restaurante/models.py
@@ -25,3 +25,27 @@ class Producto(models.Model):
 class Detalle(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     detalle =  models.TextField()
+
+class Cliente(models.Model):
+    identificacion = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    direccion = models.CharField(max_length=255)
+
+class Sede(models.Model):
+    nit = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=100)
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    sede = models.ForeignKey(Sede, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+class Item_venta(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    cantidad = models.IntegerField() 
+
+
